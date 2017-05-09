529cd314048e9fa3eb01456561aa9639ee8de82e
Kinza Drewett <kinzadrewett@gmail.com>
2017-05-09 21:50:05 +0000

    removed unneccessary php from index.php

--- index.php
+++ index.php
@@ -13,25 +13,3 @@
         <p><a href ="register.php">Click here to register</a></p>
     </body>
 </html>
-
-<?php
-//pdo stuff
-$host = 'localhost:3306';
-$db   = 'login';
-$user = 'root';
-$pass = 'root';
-$charset = 'utf8';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$opt = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-$pdo = new PDO($dsn, $user, $pass, $opt);
-
-
-
-
-
-?>
