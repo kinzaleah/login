49b49263ad97fb8c86b1b8224b8e5d6f5b03757c
Kinza Drewett <kinzadrewett@gmail.com>
2018-01-28 19:06:39 +0000

    add psysh REPL

---
+++ .psysh.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '/functions.php';
+require_once __DIR__ . '/vendor/autoload.php';
+
+echo 'Hey Kinza! Welcome to your awesome login project!' . PHP_EOL;
+
+return [];
